37b9d3c9ea0d80248f434ec8c71ccda197f3345c
Miquéias Garcia <mglucenalucena@gmail.com>
2020-03-25 20:25:59 -0300

    Back-end e Front-end da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './global.css';
+
+import Routes from './routes';
+
+// JSX (JavaScript XML)
+//quando não é preciso haver conteudo dentro da "tag" ela pode ser fechada nela mesma, não necessitando de um </ exemplo>
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
